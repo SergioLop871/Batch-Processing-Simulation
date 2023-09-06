50a2ec7183a26a1756104d87669334b8e324c1a4
jesuslugo394 <144289572+jesuslugo394@users.noreply.github.com>
2023-09-06 17:56:31 -0600

    Update batch_processing.py

--- batch_processing.py
+++ batch_processing.py
@@ -1,3 +1,4 @@
+import time 
 from tkinter import *
 
 root = Tk()
@@ -30,4 +31,101 @@ add_process.pack()
 
 
 root.mainloop()
+
+#Falta agregar sleep y limpiar pantalla
+class Proceso:
+    def __init__(self, nombre_programador, operacion, datos, tiempo_maximo, numero_programa):
+        self.nombre_programador = nombre_programador
+        self.operacion = operacion
+        self.datos = datos
+        self.tiempo_maximo = tiempo_maximo
+        self.numero_programa = numero_programa
+        self.tiempo_transcurrido = 0
+
+    def ejecutar(self):
+        resultado = None
+        if self.operacion == '+':
+            resultado = sum(self.datos)
+        elif self.operacion == '-':
+            resultado = self.datos[0] - self.datos[1]
+        elif self.operacion == '*':
+            resultado = self.datos[0] * self.datos[1]
+        elif self.operacion == '/':
+            if self.datos[1] != 0:
+                resultado = self.datos[0] / self.datos[1]
+        elif self.operacion == 'residuo':
+            resultado = self.datos[0] % self.datos[1]
+        elif self.operacion == 'porcentaje':
+            resultado = (self.datos[0] / 100) * self.datos[1]
+        return resultado
+
+def capturar_proceso():
+    nombre_programador = input("Nombre del programador: ")
+    operacion = input("Operación a realizar (+, -, *, /, residuo, porcentaje): ")
+    while operacion not in ('+', '-', '*', '/', 'residuo', 'porcentaje'):
+        print("Operación no válida. Intente de nuevo.")
+        operacion = input("Operación a realizar (+, -, *, /, residuo, porcentaje): ")
+    datos = []
+    for i in range(2):
+        dato = float(input(f"Ingrese dato {i + 1}: "))
+        datos.append(dato)
+    tiempo_maximo = float(input("Tiempo Máximo Estimado (mayor a 0): "))
+    while tiempo_maximo <= 0:
+        print("Tiempo Máximo Estimado debe ser mayor a 0. Intente de nuevo.")
+        tiempo_maximo = float(input("Tiempo Máximo Estimado (mayor a 0): "))
+    numero_programa = input("Número de Programa (ID) único: ")
+    return Proceso(nombre_programador, operacion, datos, tiempo_maximo, numero_programa)
+
+
+def ejecutar_lote(lote):
+    for proceso in lote:
+        print("\nLote en Ejecución:")
+        print(f"Numero de programa (ID): {proceso.numero_programa}")
+        print(f"Tiempo Máximo Estimado: {proceso.tiempo_maximo}")
+        print("\nProceso en Ejecución:")
+        print(f"Nombre: {proceso.nombre_programador}")
+        print(f"Operación: {proceso.operacion} {proceso.datos}")
+        tiempo_transcurrido = 0
+        while tiempo_transcurrido < proceso.tiempo_maximo:
+            resultado = proceso.ejecutar()
+            tiempo_transcurrido += 1
+            proceso.tiempo_transcurrido += 1
+            print(f"Tiempo transcurrido en ejecución: {tiempo_transcurrido}")
+            print(f"Tiempo restante por ejecutar: {proceso.tiempo_maximo - tiempo_transcurrido}")
+            if resultado is not None:
+                print(f"Resultado de la operación: {proceso.operacion} {proceso.datos} = {resultado}")
+            else:
+                print("Error: División por cero (operación no válida)")
+
+def main():
+    lotes_pendientes = []
+    reloj_global = 0
+    
+    N = int(input("Introduce la cantidad de procesos: "))
+    procesos_capturados = 0
+    lote_actual = []
+    
+    while procesos_capturados < N:
+        proceso = capturar_proceso()
+        lote_actual.append(proceso)
+        procesos_capturados += 1
+        if len(lote_actual) == 5 or procesos_capturados == N:
+            lotes_pendientes.append(lote_actual)
+            lote_actual = []
+    
+    while lotes_pendientes:
+        lote_en_ejecucion = lotes_pendientes.pop(0)
+        print("\nNúmero de lotes pendientes:", len(lotes_pendientes))
+        print("\nLote en Ejecución:")
+        for proceso in lote_en_ejecucion:
+            proceso.tiempo_transcurrido = 0
+        ejecutar_lote(lote_en_ejecucion)
+        reloj_global += sum(proceso.tiempo_maximo for proceso in lote_en_ejecucion)
+    
+    print("\n¡Todos los procesos han sido ejecutados!")
+    print("Reloj Global:", reloj_global)
+
+if __name__ == "__main__":
+    main()
+
 
